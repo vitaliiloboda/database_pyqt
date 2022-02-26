649d2d4900286d1564a7bf41262b9023d21ff8fe
VitaliiLoboda <vitalii.loboda@gmail.com>
2022-02-26 18:12:48 +0200

    lesson 5

---
+++ lesson_5/descrptrs.py
@@ -0,0 +1,17 @@
+import logging
+logger = logging.getLogger('server')
+
+
+# Дескриптор для класса серверного сокета:
+class Port:
+    def __set__(self, instance, value):
+        if not 1023 < value < 65536:
+            logger.critical(
+                f'Попытка запуска сервера с указанием неподходящего порта {value}. Допустимы адреса с 1024 до 65535.')
+            exit(1)
+        # Если порт прошёл проверку, добавляем его в список атрибутов экземпляра
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
---
+++ lesson_5/metaclasses.py
@@ -0,0 +1,67 @@
+"""
+В основе метода библиотека dis - анализ кода с помощью его дизассемблирования
+(разбор кода на составляющие: в нашем случае - на атрибуты и методы класса)
+https://docs.python.org/3/library/dis.html
+"""
+
+import dis
+
+
+# Метакласс для проверки соответствия сервера:
+class ServerMaker(type):
+    def __init__(cls, clsname, bases, clsdict):
+        # clsname - экземпляр метакласса - Server
+        # bases - кортеж базовых классов - ()
+        # clsdict - словарь атрибутов и методов экземпляра метакласса
+
+        # Список методов, которые используются в функциях класса:
+        methods = []
+        # Атрибуты, используемые в функциях классов
+        attrs = []
+
+        for func in clsdict:
+            try:
+                ret = dis.get_instructions(clsdict[func])
+            except TypeError:
+                pass
+            else:
+                for i in ret:
+                    print(i)
+                    if i.opname == 'LOAD_GLOBAL':
+                        if i.argval not in methods:
+                            methods.append(i.argval)
+                    elif i.opname == 'LOAD_ATTR':
+                        if i.argval not in attrs:
+                            attrs.append(i.argval)
+        print(methods)
+        if 'connect' in methods:
+            raise TypeError('Использование метода connect недопустимо в серверном классе')
+
+        if not ('SOCK_STREAM' in attrs and 'AF_INET' in attrs):
+            raise TypeError('Некорректная инициализация сокета.')
+
+        super().__init__(clsname, bases, clsdict)
+
+
+# Метакласс для проверки корректности клиента:
+class ClientMaker(type):
+    def __init__(cls, clsname, bases, clsdict):
+        methods = []
+        for func in clsdict:
+            try:
+                ret = dis.get_instructions(clsdict[func])
+            except TypeError:
+                pass
+            else:
+                for i in ret:
+                    if i.opname == 'LOAD_GLOBAL':
+                        if i.argval not in methods:
+                            methods.append(i.argval)
+        for command in ('accept', 'listen', 'socket'):
+            if command in methods:
+                raise TypeError('В классе обнаружено использование запрещённого метода')
+        if 'get_message' in methods or 'send_message' in methods:
+            pass
+        else:
+            raise TypeError('Отсутствуют вызовы функций, работающих с сокетами.')
+        super().__init__(clsname, bases, clsdict)
